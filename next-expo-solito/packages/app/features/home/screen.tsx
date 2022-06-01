23aabeea50312494f8a87e042aafcfdff4543f84
natew <natewienert@gmail.com>
2022-05-31 23:18:47 -1000

    upgrade tamagui

--- next-expo-solito/packages/app/features/home/screen.tsx
+++ next-expo-solito/packages/app/features/home/screen.tsx
@@ -1,29 +1,30 @@
+import { Anchor, Button, H1, Paragraph, Separator, XStack, YStack } from '@my/ui'
 import { Drawer } from '@tamagui/drawer'
 import { ChevronDown, ChevronUp } from '@tamagui/feather-icons'
 import React, { useState } from 'react'
 import { useWindowDimensions } from 'react-native'
 import { useLink } from 'solito/link'
-import { Anchor, Button, H1, Paragraph, XStack, YStack } from '@my/ui'
 
 export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
   })
-
+  
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$2" maw={600}>
+      <YStack space="$4" maw={600}>
         <H1 ta="center">Welcome to Tamagui.</H1>
         <Paragraph ta="center">
-          Here is a basic starter to show you how you can navigate from one screen to another. This
+          Here's a basic starter to show navigating from one screen to another. This
           screen uses the same code on Next.js and React Native.
         </Paragraph>
+        <Separator />
         <Paragraph ta="center">
           Tamagui is made by{' '}
-          <Anchor href="https://twitter.com/natebirdman" target="_blank" rel="noreferrer">
+          <Anchor href="https://twitter.com/natebirdman" target="_blank">
             Nate Wienert
-          </Anchor>
-          .
+          </Anchor>,
+          give it a star <Anchor href="https://github.com/tamagui/tamagui" target="_blank" rel="noreferrer">on Github</Anchor>.
         </Paragraph>
       </YStack>
 
--- next-expo-solito/packages/ui/types/MyComponent.d.ts
+++ next-expo-solito/packages/ui/types/MyComponent.d.ts
@@ -1,30 +1,30 @@
 export declare const MyComponent: import("tamagui").TamaguiComponent<(Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{}, "elevation" | "fullscreen"> & {
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 } & import("@tamagui/core").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{}, "elevation" | "fullscreen"> & {
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 }>> & import("@tamagui/core").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{}, "elevation" | "fullscreen"> & {
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 }>>) | (Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 }, string | number> & {
     [x: string]: undefined;
 } & import("@tamagui/core").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 }, string | number> & {
     [x: string]: undefined;
 }>> & import("@tamagui/core").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children"> & import("tamagui").RNWViewProps & import("tamagui").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & Omit<{
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 }, string | number> & {
     [x: string]: undefined;
 }>>), any, import("tamagui").StackPropsBase, {
-    fullscreen?: boolean | undefined;
-    elevation?: import("tamagui").SizeTokens | undefined;
+    readonly fullscreen?: boolean | undefined;
+    readonly elevation?: import("tamagui").SizeTokens | undefined;
 } & ({} | {
     [x: string]: undefined;
 })>;
